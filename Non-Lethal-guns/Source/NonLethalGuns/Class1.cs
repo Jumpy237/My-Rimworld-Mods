903a936fe63fa81d928cbf558c34f738db6cc693
Jumpy237 <naphunphul@gmail.com>
2018-01-04 21:08:11 +0700

    added all defs

---
+++ Non-Lethal-guns/Source/NonLethalGuns/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace NonLethalGuns
+{
+    public class ThingDef_Dart : Def
+    {
+         
+    }
+}
